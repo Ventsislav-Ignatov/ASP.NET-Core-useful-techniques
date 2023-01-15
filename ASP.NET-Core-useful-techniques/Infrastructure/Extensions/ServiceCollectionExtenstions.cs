a8e4e36d1f6ad73ca1cfab7a3c279db61e2a1641
Ventsislav-Ignatov <vencislav.ignatov@gmail.com>
2023-01-15 11:57:35 +0200

    refactor global service creation method

--- ASP.NET-Core-useful-techniques/Infrastructure/Extensions/ServiceCollectionExtenstions.cs
+++ ASP.NET-Core-useful-techniques/Infrastructure/Extensions/ServiceCollectionExtenstions.cs
@@ -3,50 +3,43 @@
     using ASP.NET.Core.Useful.Techniques.Common.Interfaces;
     using Microsoft.Extensions.DependencyInjection;
     using System;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
 
-    public static class ServiceCollectionExtenstions
+    internal static class ServiceCollectionExtenstions
     {
-        static ServiceCollectionExtenstions()
+        private static Type transientServiceInterfaceType = typeof(IService);
+        private static Type singletonServiceInterfaceType = typeof(ISingletonService);
+        private static Type scopedServiceInterfaceType = typeof(IScopedService);
+
+        internal static IServiceCollection AddDomainServices(this IServiceCollection services)
         {
-        }
-
-        public static IServiceCollection AddDomainServices(this IServiceCollection services)
-        {
-            var transientServiceInterfaceType = typeof(IService);
-            var singletonServiceInterfaceType = typeof(ISingletonService);
-            var scopedServiceInterfaceType = typeof(IScopedService);
-
-            var exportedTypes = transientServiceInterfaceType.Assembly.GetExportedTypes()
-                .Where(t => t.IsClass && t.GetInterfaces().Any(i => i.Name == $"I{t.Name}") && !t.IsAbstract && !t.IsGenericType)
-                .Select(t => new
-                {
-                    Interface = t.GetInterface($"I{t.Name}"),
-                    Implementation = t
-                })
-                .Where(t => t.Interface != null)
-                .ToList();
-
-            foreach (var service in exportedTypes)
-            {
-                if (transientServiceInterfaceType.IsAssignableFrom(service.Interface))
-                {
-                    services.AddTransient(service.Interface, service.Implementation);
-                }
-                else if (singletonServiceInterfaceType.IsAssignableFrom(service.Implementation))
-                {
-                    services.AddSingleton(service.Interface, service.Implementation);
-                }
-                else if (scopedServiceInterfaceType.IsAssignableFrom(service.Implementation))
-                {
-                    services.AddScoped(service.Interface, service.Implementation);
-                }
-            }
-
+            Assembly
+               .GetAssembly(transientServiceInterfaceType)
+               .GetTypes()
+               .Where(t => t.IsClass && t.GetInterfaces().Any(i => i.Name == $"I{t.Name}") && !t.IsAbstract && !t.IsGenericType)
+               .ToList()
+               .ForEach(x => services.AddService(x));
 
             return services;
+        }
+
+        private static Type Interface(this Type type) => type.GetInterface($"I{type.Name}");
+
+        private static void AddService(this IServiceCollection services, Type currentService)
+        {
+            if (transientServiceInterfaceType.IsAssignableFrom(currentService))
+            {
+                services.AddTransient(currentService.Interface(), currentService);
+            }
+            else if (singletonServiceInterfaceType.IsAssignableFrom(currentService))
+            {
+                services.AddSingleton(currentService.Interface(), currentService);
+            }
+            else if (scopedServiceInterfaceType.IsAssignableFrom(currentService))
+            {
+                services.AddScoped(currentService.Interface(), currentService);
+            }
         }
     }
 }
